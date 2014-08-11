e5a74303d3c594d5ac292fcae71cee721efa52d0
John Smith <rjdj7@yahoo.com>
2014-08-11 16:58:54 -0400

    Calculations pg. 27 exercise

--- AssociatingJS/script.js
+++ AssociatingJS/script.js
@@ -1,3 +1,11 @@
-var TotalCostOfShopping;
-TotalCostOfShopping = 10 + 5 + 5;
-alert (TotalCostOfShopping);
+var firstNumber = 15;
+var secondNumber = 10;
+var answer;
+answer = 15 / 10;
+alert (answer);
+
+alert (15 / 10);
+
+answer = firstNumber / secondNumber;
+alert (answer);
+
