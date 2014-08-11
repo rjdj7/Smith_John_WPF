27ee1187b17e8674031e41616b21ee948eeeb59c
John Smith <rjdj7@yahoo.com>
2014-08-11 16:45:14 -0400

    Numerical Calculations exercise

--- AssociatingJS/script.js
+++ AssociatingJS/script.js
@@ -1,7 +1,3 @@
-//Arrays
-
-var muppetNames = ["Kermit", "Miss Piggy", "Gonzo"; "Rizzo"]; //0,1,2,3,4
-
-console.log(muppetNames);
-
-
+var TotalCostOfShopping;
+TotalCostOfShopping = 10 + 5 + 5;
+alert (TotalCostOfShopping);
