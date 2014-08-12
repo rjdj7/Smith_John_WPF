6bc099c0300ac461121b2618cb796227fb5769fd
John Smith <rjdj7@yahoo.com>
2014-08-12 14:08:11 -0400

    Added formula and print result.

--- Expressions_worksheet/script.js
+++ Expressions_worksheet/script.js
@@ -2,10 +2,14 @@
 // 08/12/14
 // Expressions Worksheet
 
-// Dog Years
+// Problem 1: Dog Years
 
-// Declare my variables:
-var humanyear;
-var dogyear;
+// Declare my variables. regYear is Sparky's age in regular years. dogYear is Sparky's age in dog years. 
+var regYear;
+var dogYear;
 
-// 
+// The forumla for converting regular (human) years to dog years:
+dogYear = regYear * 7 
+
+// Pring the result in the console:
+console.log(dogYear)
