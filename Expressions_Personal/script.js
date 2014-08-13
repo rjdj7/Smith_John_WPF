44ece3e3cc69be55256b89d1582f89b381a381f9
John Smith <rjdj7@yahoo.com>
2014-08-13 14:36:00 -0400

    Edited comments. Added formula for pages per hour.

--- Expressions_Personal/script.js
+++ Expressions_Personal/script.js
@@ -5,11 +5,12 @@
 // 758 pages in Beginning JavaScript book. To finish the book, how many pages would I need to read a day for 30 days? How many pages per hour? 
 
 // Declaring the givens. 758 pages. 30 days.
-var bookPages = 758
-var days = 30
-var hoursInDay = 24
+var bookPages = 758;
+var days = 30;
+var hoursInDay = 24;
 
-// Formula for the amount of pages per day.
-var pagesPerDay = bookPages / days
+// Formula for the amount of pages per day, which is the amount of pages in the book divided into 30 days. 
+var pagesPerDay = bookPages / days;
 
-//
+// Forumla for amount of pages per hour, which is the amount of pages in a day divided by the hours in a day. 
+var pagesPerHour = pagesPerDay / hoursInDay;
