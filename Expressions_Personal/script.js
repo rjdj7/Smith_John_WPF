58df000a3b11bda8e4a0487eeb74bcf5c228ae51
John Smith <rjdj7@yahoo.com>
2014-08-13 15:03:46 -0400

    Took away the word problem. Added user prompts. Added an alert.

--- Expressions_Personal/script.js
+++ Expressions_Personal/script.js
@@ -2,11 +2,9 @@
 // 08/13/14
 // Expressions Personal
 
-// 758 pages in Beginning JavaScript book. To finish the book, how many pages would I need to read a day for 30 days? How many pages per hour? 
-
 // Declaring the givens. 758 pages. 30 days.
-var bookPages = 758;
-var days = 30;
+var bookPages = prompt("Please enter the amount of pages in your book:");
+var days = prompt("Now enter the amount of days you wish to finish it in:");
 var hoursInDay = 24;
 
 // Formula for the amount of pages per day, which is the amount of pages in the book divided into 30 days. 
@@ -14,3 +12,6 @@ var pagesPerDay = bookPages / days;
 
 // Forumla for amount of pages per hour, which is the amount of pages in a day divided by the hours in a day. 
 var pagesPerHour = pagesPerDay / hoursInDay;
+
+// Alert to notify the user how many pages per day it would take to finish their book. 
+alert("You would have to read " + pagesPerDay + " pages per day to finish the book. That's " + pagesPerHour + " pages an hour!");
