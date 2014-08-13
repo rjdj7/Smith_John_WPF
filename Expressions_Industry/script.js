2a832d9db818051cd73f47cf5099a37796975c72
John Smith <rjdj7@yahoo.com>
2014-08-13 15:30:26 -0400

    Declaring prompts.

--- Expressions_Industry/script.js
+++ Expressions_Industry/script.js
@@ -1,3 +1,13 @@
 // John Smith
 // 08/13/14
 // Expressions Industry
+
+// How many webpages do you have to code total?
+// How many have you coded so far? 
+// How many hours do you have available to you? 
+
+// Declaring prompts:
+var totalPages = prompt("How many webpages do you have to code total?")
+var soFar = prompt("How many have you coded so far?")
+var hoursAvail = prompt("How many hours do you have available to you?")
+
