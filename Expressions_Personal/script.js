041d76560d9fbcc060344693376fbd92682fa0e2
John Smith <rjdj7@yahoo.com>
2014-08-13 14:30:27 -0400

    Declaring the givens.

--- Expressions_Personal/script.js
+++ Expressions_Personal/script.js
@@ -1,3 +1,12 @@
 // John Smith
 // 08/13/14
 // Expressions Personal
+
+// 758 pages in Beginning JavaScript book. To finish the book, how many pages would I need to read a day for 30 days? How many pages per hour? 
+
+// Declaring the givens. 758 pages. 30 days.
+var bookPages = 758
+var days = 30
+var hoursInDay = 24
+
+//
