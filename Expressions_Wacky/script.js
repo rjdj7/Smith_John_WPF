1ff9f7d1c44e875093261d8507049216b7b8b4d8
John Smith <rjdj7@yahoo.com>
2014-08-14 12:41:31 -0400

    Parsed the zombiesKilled and humansKilled variables to fix an error.

--- Expressions_Wacky/script.js
+++ Expressions_Wacky/script.js
@@ -10,7 +10,7 @@ var zombiesKilled = prompt("There are 200 million zombies in the US. You're a zo
 var humansKilled = prompt("The undead aren't the only threat out there...How many of the living have you killed?");
 
 // Declaring the forumla for total killed, which is the zombies plus the humans that user has killed. Also, added alert for this:
-var totalKilled = zombiesKilled + humansKilled;
+var totalKilled = parseInt(zombiesKilled) + parseInt(humansKilled);
 alert("Wow. So you've killed a total of " + totalKilled);
 
 // Adding variable for the number of humans to be reanimated. 
@@ -18,3 +18,5 @@ var humansReanimated = humansKilled / 2;
 
 // Adding new zombie population after the zombies killed have been subtracted and the humans reanimated have been added:
 var zombiePopNew = zombiePop - zombiesKilled + humansReanimated;
+
+alert("Ok, so you've killed " + zombiesKilled + " zombies and " + humansKilled + " humans. " + humansReanimated + " of which have reanimated, making the total zombie population now " + zombiePopNew);
