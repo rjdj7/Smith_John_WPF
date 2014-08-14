cec17a8f4013b404f25e583c46395cd88c5270a1
John Smith <rjdj7@yahoo.com>
2014-08-14 12:18:27 -0400

    Initial setup. Declared beginning prompts: zombiesKilled, humansKilled.

--- Expressions_Wacky/script.js
+++ Expressions_Wacky/script.js
@@ -1,3 +1,11 @@
 // John Smith
 // 08/13/14
 // Expressions Wacky
+
+// You're a zombie slayer. How many have you killed? Total killed. Chances of animating 1 in 2. 
+
+// Declaring some prompts:
+var zombiesKilled = prompt("There are 200 million zombies in the US. You're a zombie slayer. How many have you killed?")
+var humansKilled = prompt("The undead aren't the only threat out there...How many of the living have you killed?")
+
+// 
