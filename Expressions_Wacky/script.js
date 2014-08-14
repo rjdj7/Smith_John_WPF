db32654e83cf4d9bd1fdd7ebb912950a7892c4ff
John Smith <rjdj7@yahoo.com>
2014-08-14 12:22:38 -0400

    Added alert for totalKilled.

--- Expressions_Wacky/script.js
+++ Expressions_Wacky/script.js
@@ -8,7 +8,8 @@
 var zombiesKilled = prompt("There are 200 million zombies in the US. You're a zombie slayer. How many have you killed?");
 var humansKilled = prompt("The undead aren't the only threat out there...How many of the living have you killed?");
 
-// Declaring the forumla for total killed, which is the zombies plus the humans that user has killed:
+// Declaring the forumla for total killed, which is the zombies plus the humans that user has killed. Also, added alert for this:
 var totalKilled = zombiesKilled + humansKilled;
+alert("Wow. So you've killed a total of " + totalKilled);
 
-//
+// 
