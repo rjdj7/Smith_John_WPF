27f4c3bf44f653e2b5adf8a9b88d61fa583a019a
John Smith <rjdj7@yahoo.com>
2014-08-14 12:20:25 -0400

    Added var "totalKilled," with comments.

--- Expressions_Wacky/script.js
+++ Expressions_Wacky/script.js
@@ -2,10 +2,13 @@
 // 08/13/14
 // Expressions Wacky
 
-// You're a zombie slayer. How many have you killed? Total killed. Chances of animating 1 in 2. 
+// You're a zombie slayer. How many have you killed? Total killed. Chances of animating 1 in 2. Zombies in the US. 
 
 // Declaring some prompts:
-var zombiesKilled = prompt("There are 200 million zombies in the US. You're a zombie slayer. How many have you killed?")
-var humansKilled = prompt("The undead aren't the only threat out there...How many of the living have you killed?")
+var zombiesKilled = prompt("There are 200 million zombies in the US. You're a zombie slayer. How many have you killed?");
+var humansKilled = prompt("The undead aren't the only threat out there...How many of the living have you killed?");
 
-// 
+// Declaring the forumla for total killed, which is the zombies plus the humans that user has killed:
+var totalKilled = zombiesKilled + humansKilled;
+
+//
