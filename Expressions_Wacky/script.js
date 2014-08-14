326a97211f86dd6e78dd8fcaeb48925815862524
John Smith <rjdj7@yahoo.com>
2014-08-14 12:30:06 -0400

    Added zombiePopNew variable.

--- Expressions_Wacky/script.js
+++ Expressions_Wacky/script.js
@@ -5,6 +5,7 @@
 // You're a zombie slayer. How many have you killed? Total killed. Chances of animating 1 in 2. Zombies in the US. 
 
 // Declaring some prompts:
+var zombiePop = 200000000;
 var zombiesKilled = prompt("There are 200 million zombies in the US. You're a zombie slayer. How many have you killed?");
 var humansKilled = prompt("The undead aren't the only threat out there...How many of the living have you killed?");
 
@@ -15,4 +16,5 @@ alert("Wow. So you've killed a total of " + totalKilled);
 // Adding variable for the number of humans to be reanimated. 
 var humansReanimated = humansKilled / 2; 
 
-//
+// Adding new zombie population after the zombies killed have been subtracted and the humans reanimated have been added:
+var zombiePopNew = zombiePop - zombiesKilled + humansReanimated;
