ffbf982340773801d8eeb1036f3779ef478d5a3a
John Smith <rjdj7@yahoo.com>
2014-08-20 17:17:34 -0400

    Conditional Logic Screencast

--- AssociatingJS/script.js
+++ AssociatingJS/script.js
@@ -1,8 +1,20 @@
-// Prompt and Alert
+//Conditional Logic
 
-var width = prompt("We are calculating the area of a rectangle. Please enter the width");
-var height = prompt("Please enter the height.");
-var area = width * height;
-//console.log(area); 
+var kidHeight = 47;
+var minHeight = 48;
+var sneakerLift = 2;
+var wParentHeight = 45; //the height of the kid with the parent
 
-alert("The area of your rectangle is " + area); // Note: Alerts are outdated 
+//if the child is old enough, print to the console: "you can ride!"
+//if the kid is over 48 inches in height
+if(kidHeight + sneakerLift > minHeight){
+	//you can ride
+	console.log("You can ride the coaster!");
+}else if (kidHeight > wParentHeight){
+	//you can ride with a parent present
+	console.log("You can ride, but only with a parent present.");
+}else{
+	//sorry you have growing to do
+	console.log("Sorry kid, you've got some growing to do first!");
+}
+
