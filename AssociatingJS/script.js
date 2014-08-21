d36dfb444b930ce1512d966602a04cf77c037b4a
John Smith <rjdj7@yahoo.com>
2014-08-20 22:27:54 -0400

    Ternary Operators - Edit 2

--- AssociatingJS/script.js
+++ AssociatingJS/script.js
@@ -4,10 +4,6 @@ var age = 11;
 var book;
 
 //If the child is under 10, they get Green Eggs and Ham, otherwise, they get The Time Machine
-if(age < 10){
-	book = "Green Eggs and Ham";
-}else{
-	book = "The Time Machine";
-}
-
+book = (age < 10) ? "Green Eggs and Ham" : "The Time Machine"; // Colon means otherwise
 console.log(book);
+
