475a56c7d068d0b2b114e01654a8855699c6432d
John Smith <rjdj7@yahoo.com>
2014-08-20 21:46:56 -0400

    Conditional Logic- logical operators. The or ||

--- AssociatingJS/script.js
+++ AssociatingJS/script.js
@@ -2,10 +2,10 @@
 
 var budget = 300;
 var iPhonePrice = 199.99;
-var paycheck = 200;
+var wonLottery = true;
 
 //if the price of the phone is less than our budget and if our paycheck is over 300:
-if(iPhonePrice < budget && paycheck > 300){
+if(iPhonePrice < budget || wonLottery){
 	console.log("We can buy the phone!");
 }else{
 	console.log("No phone for you!");
