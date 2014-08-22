edeb1629f6cf32de418391fdef45fe67f9a9384f
John Smith <rjdj7@yahoo.com>
2014-08-21 20:32:59 -0400

    Ch. 3 pg. 61: Multiple Conditions

--- AssociatingJS/script.js
+++ AssociatingJS/script.js
@@ -1,24 +1,28 @@
-//Ch. 3 pg. 55 The if Statement Exercise
+//Ch. 3 pg. 61: Multiple Conditions 
 
-//Declaring the degrees fahrenheit and degrees centigrade variables.
-//A prompt will appear for the variable degFahren. The user must enter a number. The default value is 32. 
-//"Number() is used to parse the users input, which is techinically a string."
-var degFahren = Number(prompt("Enter the degrees Fahrenheit", 32));
-var degCent;
+//Prompting the user to enter their age. Default value is 30:
+var myAge = Number(prompt("Enter your age", 30));
 
-//Fahrenheit to Centigrade converstion formula:
-degCent = 5/9 * (degFahren - 32);
+//If the age is greater than or equal to zero AND less than or equal to 10, then...
+if (myAge >= 0 && myAge <=10){
 
-//Write the result onto the page:
-document.write(degFahren + "\xB0 Fahrenheit is " + degCent + "\xB0 centigrade<br/>");
-
-//If the value of degCent is less than zero then...
-if (degCent < 0) {
-	document.write("That's below the freezing point of water.");
-
+	document.write("My age is between 0 and 10 <br/>");
 }
 
-//If the value of degCent is equal to 100 then...
-if (degCent == 100){
-	document.write("That's the boiling point of water.");
+//The NOT operator reverses this otherwise false statement, making it true. (Because 30 is out of the age range.)
+if (!(myAge >= 0 && myAge <= 10)){
+
+	document.write("My age is not between 0 and 10 </br>");
+}
+
+//If the age is greater than or equal to 80 OR the age is less than or equal to 10, then...
+if (myAge >= 80 || myAge is <= 10){
+
+	document.write("My age is 80 or above OR 10 or below <br/>");
+}
+
+//If the age is greater than or equal to 30 and less than or equal to 39 OR if the age is greater than or equal to 80 and less than or equal to 89, then...
+if (myAge >= 30 && myAge <= 39) || (myAge >= 80 && myAge <= 89){
+
+	document.write("myAge is between 30 and 39 or myAge is between 80 and 89");
 }
