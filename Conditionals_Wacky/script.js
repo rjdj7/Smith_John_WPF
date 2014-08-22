0c1d13af22c60f93f7b6f0e4627de5db844fe00f
John Smith <rjdj7@yahoo.com>
2014-08-22 15:05:10 -0400

    Assignment Conditionals: Wacky initial setup.

--- Conditionals_Wacky/script.js
+++ Conditionals_Wacky/script.js
@@ -1,3 +1,26 @@
 // John Smith
 // 08/21/14
 // Assignment: Conditionals
+
+//Wacky: Apocalypse
+
+//It's the apocalypse! What happened!?
+//War, Zombies, Aliens, Natural Disaster
+
+var whatHappened = Number(prompt("It's the apocalypse! What happened!? Was it: 1. War. 2. Zombies 3. Aliens 4. Natural Disaster. Insert a number:"));
+
+//Different answers based on what happened. 
+if (whatHappened === 1){
+	console.log();
+}else if (whatHappened === 2){
+	console.log();
+}else if (whatHappened === 3){
+	console.log();
+}else if (whatHappened === 4){
+	console.log();
+}
+
+//Validation
+if (whatHappened === ""){
+	console.log("Please insert a number. 1, 2, 3, or 4.");
+}
