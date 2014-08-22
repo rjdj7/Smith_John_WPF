a5528d162f125bf6f4f011981c98fded72c56784
John Smith <rjdj7@yahoo.com>
2014-08-22 10:56:38 -0400

    Last Chance for Gas Problem Completed.

--- Conditionals_Worksheet/script.js
+++ Conditionals_Worksheet/script.js
@@ -2,15 +2,21 @@
 // 08/21/14
 // Conditionals Worksheet
 
+//Last Chance for Gas!
+
 //Declaring givens and assigning values:
 var mpg = 25; //miles per gallon
-var gasLeft = tankCap / 2; //50% of gas tank (9 gallons left)
+var gasLeft = 9; //50% of gas tank 
 var tankCap = 18; //car's tank capacity
 var milesLeft = 200; //the miles left
 
 //Forumula for gas needed for the remainder of the trip:
 var gasNeeded = milesLeft / mpg;
 
-if(tankCap >= gasNeeded){
+//If gas left is greater than or equal to the gas needed for the trip then:
+if(gasLeft >= gasNeeded){
 	console.log("Yes, you can make it without stopping for gas!");
+}else{
+	console.log("You only have " + gasLeft + " gallons of gas in your tank, better get gas now while you can!");
 }
+
