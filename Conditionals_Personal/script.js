3b0811c95859efed819d1b6bb4af8be9554ea3e1
John Smith <rjdj7@yahoo.com>
2014-08-22 14:18:24 -0400

    Finished Personal SubProject for Assignment: Conditionals.

--- Conditionals_Personal/script.js
+++ Conditionals_Personal/script.js
@@ -8,12 +8,30 @@
 var firstName = prompt("Please enter your first name:");
 var lastName = prompt("Please enter your last name:");
 var email = prompt("Please enter your email address:");
-var age = prompt("Please enter your age:");
+var age = Number(prompt("Please enter your age:"));
 
 //Correct info:
 var correctFName = "John";
 var correctLName = "Smith";
 var correctEmail = "rjdj7@yahoo.com";
 
-//
- 
+//If the first and last names are incorrect: invalid name. If email is: invalid email. 
+//If the age is under 18: must be 18 or over. Otherwise: Thank you
+if(firstName != correctFName && lastName != correctLName){
+	console.log("Invalid name.");
+ }else if(email != correctEmail){
+ 	console.log("Invalid email.");
+ }else if(age < 18){
+ 	console.log("Sorry, must be 18 or older to subscribe to our newsletter.");
+ }else{
+ 	console.log("Thank you. You have successfully subscribed to our newsletter.")
+ }
+
+ //Validation:
+if(firstName = "" || lastName = ""){
+	console.log("Please fill out both your first and last name.");
+}else if(email = ""){
+	console.log("Please fill out your email.");
+}else if(age = ""){
+	console.log("Please fill out your age.");
+}
