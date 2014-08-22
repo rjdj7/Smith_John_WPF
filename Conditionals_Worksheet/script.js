1157a5f1672740bd3e7d6bfbacc5f6b906b2b6b2
John Smith <rjdj7@yahoo.com>
2014-08-22 10:15:21 -0400

    Assigning values to givens.

--- Conditionals_Worksheet/script.js
+++ Conditionals_Worksheet/script.js
@@ -2,8 +2,10 @@
 // 08/21/14
 // Conditionals Worksheet
 
-//Declaring givens:
-var mpg;
-var gasLeft;
-var tankCap;
+//Declaring givens and assigning values:
+var mpg = 25; //miles per gallon
+var gasLeft = tankCap / 2; //50% of gas tank
+var tankCap = 18; //gallons
+
+
 
