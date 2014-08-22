0a66c09d915790304b4acc9130812567b3147f3b
John Smith <rjdj7@yahoo.com>
2014-08-22 15:15:25 -0400

    Assignment Conditionals: Wacky completed.

--- Conditionals_Wacky/script.js
+++ Conditionals_Wacky/script.js
@@ -11,16 +11,16 @@ var whatHappened = Number(prompt("It's the apocalypse! What happened!? Was it: 1
 
 //Different answers based on what happened. 
 if (whatHappened === 1){
-	console.log();
+	console.log("Grab a gun and prepare to fight. Chances are, no place is safe.");
 }else if (whatHappened === 2){
-	console.log();
+	console.log("Stock up on supplies and stay barricaded in your home until things cool down. When supplies begin to dwindle, find your nearest military base and pray that it's still standing.");
 }else if (whatHappened === 3){
-	console.log();
+	console.log("Keep out of the open and hope that law enforcement deals with this new threat.");
 }else if (whatHappened === 4){
-	console.log();
+	console.log("You're pretty much screwed");
 }
 
 //Validation
-if (whatHappened === ""){
+if (whatHappened == ""){
 	console.log("Please insert a number. 1, 2, 3, or 4.");
 }
