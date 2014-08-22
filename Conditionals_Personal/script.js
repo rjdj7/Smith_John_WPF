b02ea3a41b8a7828eaba6832479f49f6b7dcad33
John Smith <rjdj7@yahoo.com>
2014-08-22 13:52:54 -0400

    Added Correct Info variables for user prompt.

--- Conditionals_Personal/script.js
+++ Conditionals_Personal/script.js
@@ -2,10 +2,18 @@
 // 08/21/14
 // Assignment: Conditionals
 
-//Personal: Subscribing to a newsletter.
+//Personal: Subscribing to an adult newsletter.
 
-//First Name, Last Name, E-mail Address
+//First Name, Last Name, E-mail Address, Age:
 var firstName = prompt("Please enter your first name:");
 var lastName = prompt("Please enter your last name:");
 var email = prompt("Please enter your email address:");
+var age = prompt("Please enter your age:");
 
+//Correct info:
+var correctFName = "John";
+var correctLName = "Smith";
+var correctEmail = "rjdj7@yahoo.com";
+
+//
+ 
