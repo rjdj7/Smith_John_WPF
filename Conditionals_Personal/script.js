5c9ca19e713375ee3b9864ff6ff3c0a9e0b526d9
John Smith <rjdj7@yahoo.com>
2014-08-22 13:48:19 -0400

    Declaring givens for Personal.

--- Conditionals_Personal/script.js
+++ Conditionals_Personal/script.js
@@ -1,3 +1,11 @@
 // John Smith
 // 08/21/14
 // Assignment: Conditionals
+
+//Personal: Subscribing to a newsletter.
+
+//First Name, Last Name, E-mail Address
+var firstName = prompt("Please enter your first name:");
+var lastName = prompt("Please enter your last name:");
+var email = prompt("Please enter your email address:");
+
