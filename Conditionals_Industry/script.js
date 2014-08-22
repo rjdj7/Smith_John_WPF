7b45c547192ccb04f0e7b978a7da833f46f38a2a
John Smith <rjdj7@yahoo.com>
2014-08-22 14:53:18 -0400

    Finished Industry Subproject for Assignment: Conditionals

--- Conditionals_Industry/script.js
+++ Conditionals_Industry/script.js
@@ -1,3 +1,17 @@
 // John Smith
 // 08/21/14
 // Assignment: Conditionals
+
+//Industry: Charging Clients
+
+//Declaring variables for the site's budget and the client's budget:
+var siteBudget = 10000;
+var clientBudget = Number(prompt("The site will likely cost around $" + siteBudget + ". What is the client willing to pay you?"));
+
+//Used a ternary operator to say if the client budget is lower than the siteBudget, to find a new client. Else, get to work.
+console.log = clientBudget < siteBudget ? "Find a new client." : "Sounds good. Get to work.";
+
+//Validation
+if (clientBudget === ""){
+	console.log("Please do not leave field blank. Try again.");
+}
