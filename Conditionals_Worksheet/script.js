667fedb4fd68eb2a5c3f76652464e39ace82f146
John Smith <rjdj7@yahoo.com>
2014-08-22 10:45:53 -0400

    Formula for gas needed added. Other values...

--- Conditionals_Worksheet/script.js
+++ Conditionals_Worksheet/script.js
@@ -4,8 +4,13 @@
 
 //Declaring givens and assigning values:
 var mpg = 25; //miles per gallon
-var gasLeft = tankCap / 2; //50% of gas tank
-var tankCap = 18; //gallons
+var gasLeft = tankCap / 2; //50% of gas tank (9 gallons left)
+var tankCap = 18; //car's tank capacity
+var milesLeft = 200; //the miles left
 
+//Forumula for gas needed for the remainder of the trip:
+var gasNeeded = milesLeft / mpg;
 
-
+if(tankCap >= gasNeeded){
+	console.log("Yes, you can make it without stopping for gas!");
+}
