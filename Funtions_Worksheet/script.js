38a5599d8d24a66fac64adce47474a61d69db596
John Smith <rjdj7@yahoo.com>
2014-08-25 21:48:56 -0400

    Functions Worksheet HTML page setup

---
+++ Funtions_Worksheet/script.js
@@ -0,0 +1,10 @@
+//Functions - Parameter and Arguments
+
+function dogYears(age){//parameters
+	var dogYears = age * 7;
+	console.log("Sparky is " + dogYears + "years old.");
+}
+
+//Arguments
+dogYears(4); //Making the value of age 4 making the anser 28
+dogYears(5); //Making the value of age 5 making the answer 35
