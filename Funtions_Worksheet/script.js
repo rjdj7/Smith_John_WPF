65fce9326a966df2aa6c40244557c6413e61adfb
John Smith <rjdj7@yahoo.com>
2014-08-25 22:19:20 -0400

    Functions Worksheet - Circumference

--- Funtions_Worksheet/script.js
+++ Funtions_Worksheet/script.js
@@ -1,4 +1,20 @@
 // John Smith
 // 08/25/14
-// Assignment: Functions Worksheet
+// Assignment: Functions Worksheet - Circumference
 
+//C = 2 * 3.14 * r
+
+//The arguments. What's being stored in the parameters. 
+//The forumula for the circumference of a circle
+circumference (2, 3.14, 90);
+
+//Declaring the function and added the parameters:
+function circumference(two, pie, radius){
+	var circum = two * pie * radius;
+	console.log(circum);
+
+	return circum;
+}
+
+//Calling the function:
+circumference();
