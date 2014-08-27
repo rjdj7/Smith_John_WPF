a763a5396fd7ef9d4085e7ae2a0e6f9a13a2cb64
John Smith <rjdj7@yahoo.com>
2014-08-27 16:15:30 -0400

    Functions: Wacky

--- Functions_ Wacky/script.js
+++ Functions_ Wacky/script.js
@@ -1,3 +1,15 @@
 // John Smith
 // 08/27/14
 // Assignment: Functions Wacky
+
+//Experience Points
+
+//Anonymous function:
+var expPoints = function(){ //parameters
+	var hits = 10;
+	var expPoints = hits * 2000;
+	console.log("You have gained a total of " + expPoints + " exp." );
+}
+
+//Ternary. If the exp earned is greater than 1000, say Congrats, if not: keep training:
+console.log = expPoints > 5000 ? "Congrats. You've leveled up." : "Keep training.";
